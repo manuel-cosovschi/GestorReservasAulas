d95c7ce5605bbe84c4c6aa8b79a098f915ca415e
Joaquin Garcia <joaquingarcia7596@gmail.com>
2024-06-07 01:26:32 -0300

    Joaquin - 1ra version del carga txt

--- GestorReservasAulas/src/Main.java
+++ GestorReservasAulas/src/Main.java
@@ -1,6 +1,19 @@
+import gestionReservasAulas.controlador.GestionReservas;
+import gestionReservasAulas.util.CargaDatos;
+
+import java.io.File;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        // Crear una instancia de la clase GestionReservas
+        GestionReservas gestionReservas = new GestionReservas();
+        // Crear una instancia de la clase CargaDatos pasando gestionReservas
+        CargaDatos cargaDatosTxt = new CargaDatos(gestionReservas);
+        // Cargar datos desde un archivo
+        File archivo = new File("GestorReservasAulas/archivoAula.txt");
+        cargaDatosTxt.cargarDatos(archivo);
+    }
 }
 
-//Cambiar el nombre de la clase abstracta "Curso" por uno mas representativo.
-//cantReservas = (asignatura.getFechaFin().getDayOfYear() - asignatura.getFechaInicio().getDayOfYear()) / 7; Consultar si dias totales asignaturas es multiplo de 7
+// Cambiar el nombre de la clase abstracta "Curso" por uno mas representativo.
+// cantReservas = (asignatura.getFechaFin().getDayOfYear() - asignatura.getFechaInicio().getDayOfYear()) / 7; Consultar si dias totales asignaturas es multiplo de 7
--- GestorReservasAulas/src/gestionReservasAulas/util/CargaDatos.java
+++ GestorReservasAulas/src/gestionReservasAulas/util/CargaDatos.java
@@ -1,14 +1,64 @@
 package gestionReservasAulas.util;
-import java.io.File;
-import java.util.List;
+import gestionReservasAulas.controlador.GestionReservas;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
 
 public class CargaDatos {
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private GestionReservas gestionReservas;
+
+    // constructor
+    public CargaDatos(GestionReservas gestionReservas) {
+        this.gestionReservas = gestionReservas;
+    }
 
     public void cargarDatos(File archivo) {
 
+        if (!archivo.exists()) {
+            System.err.println("Error: El archivo no existe - " + archivo.getAbsolutePath());
+            return;
+        }
+        if (!archivo.canRead()) {
+            System.err.println("Error: No se puede leer el archivo - " + archivo.getAbsolutePath());
+            return;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            System.out.println("br.readLine(): " + br.readLine());
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(",");
+                switch (datos[0]) {
+                    case "Aula":
+                        System.out.println("El aula es: " + datos[0]);
+                        break;
+                    case "Asignatura":
+                        System.out.println("El asignatura es: " + datos[0]);
+                        break;
+                    case "CursoExtension":
+                        System.out.println("El Curso Extension es: " + datos[0]);
+                        break;
+                    case "Evento":
+                        System.out.println("El evento es: " + datos[0]);
+                        break;
+                    case "EventoExterno":
+                        System.out.println("El evento es: " + datos[0]);
+                        break;
+                    default:
+                        System.out.println("Tipo de dato desconocido: " + datos[0]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("IOException");
+            throw new RuntimeException(e);
+        }
     }
 
-    private void validarDatos(List<String> datos) {
-    }
+    // private void validarDatos(List<String> datos) {
+    // }
 
 }
